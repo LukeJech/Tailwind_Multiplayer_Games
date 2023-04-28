a3c8df691f93273f21c310a302db6eacf1d979f0
Luke Jech <129018706+LukeJech@users.noreply.github.com>
2023-04-28 16:59:57 -0700

    Add files via upload
    test 2

---
+++ Tailwind_Multiplayer_Games/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'baby-blue': '#8ecae6',
+        'medium-blue': '#219ebc',
+        'dark-blue': '#023047',
+        'yellow': '#ffb703',
+        'orange': '#fb8500',
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
